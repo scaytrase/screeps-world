9a1b63e7de9cbf5c8a1b9819da06bc075064afcd
Pavel Batanov <pavel@batanov.me>
2019-11-10 23:14:49 +0300

    Tune configs

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 export const HARVESTERS_COUNT = 14;
 export const HARVESTER_BODY = [WORK, CARRY, CARRY, MOVE, MOVE];
 
-export const BUILDERS_COUNT = 3;
+export const BUILDERS_COUNT = 4;
 export const BUILDERS_ENERGY_LIMIT = 100;
 export const BUILDER_BODY = [WORK, WORK, CARRY, MOVE, MOVE];
 
 export const UPGRADERS_COUNT = 1;
-export const UPGRADERS_ENERGY_LIMIT = 200;
+export const UPGRADERS_ENERGY_LIMIT = 50;
 export const UPGRADER_BODY = [WORK, CARRY, MOVE];
 
 export const REPAIRERS_COUNT = 1;
